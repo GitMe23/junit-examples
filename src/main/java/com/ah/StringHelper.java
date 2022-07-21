abf75aa85a3caadf8ac0c36bd8c8d9b862ce69d3
Andy Hopwood <andyhopwood@andys-mbp.home>
2022-07-21 11:01:32 +0100

    initial commit

---
+++ src/main/java/com/ah/StringHelper.java
@@ -0,0 +1,16 @@
+package com.ah;
+
+public class StringHelper {
+
+    public String truncateAinFirstTwoLetters(String str) {
+
+        if (str.length() <=2 ) {
+            return str.replaceAll("A", "");
+        }
+
+        String firstTwoLetters = str.substring(0,2);
+        String restOfString = str.substring(2);
+
+        return firstTwoLetters.replaceAll("A", "") + restOfString;
+    }
+}
---
+++ src/test/java/com/ah/StringHelperTest.java
@@ -0,0 +1,41 @@
+package com.ah;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+public class StringHelperTest {
+
+    StringHelper stringHelper = new StringHelper();
+
+
+//    Checking that first two letters are truncated if A
+    @Test
+    @DisplayName("Check Stringhelper works ")
+    public void testStringHelper() {
+        Assertions.assertEquals("CD", stringHelper.truncateAinFirstTwoLetters("AACD"));
+    }
+
+//    COMPARING ARRAYS
+    @Test
+    @DisplayName("Testing array equality")
+    public void testArrayComparison(){
+        int[] randomArray = {12, 4, 3, 1};
+        int[] expected = {1,3,4,12};
+        Arrays.sort(randomArray);
+        Assertions.assertArrayEquals(expected, randomArray);
+    }
+
+//    TESTING FOR EXCEPTION THROWN
+    @Test(expected = NullPointerException.class)
+    @DisplayName("Checking test passes if NullPointerException thrown")
+    public void testNullPointerException() {
+        int[] arrayWithNull = null;
+        Arrays.sort(arrayWithNull);
+    }
+
+}
